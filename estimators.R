78792c8d0654d8e047a412ca3caaf0987a67a030
Evan Munro <evan.munro@gmail.com>
2020-01-19 19:06:27 -0800

    estimate jointly, either side of cutoff

--- estimators.R
+++ estimators.R
@@ -2,10 +2,10 @@
 
 rdd_IK <- function(Y,X, c=0) {
   model <- rdd::RDestimate(y~x,data.frame(y=Y,x=X),cutpoint=c)
-  ate <- as.numeric(model$est[1]) 
+  ate <- as.numeric(model$est[1])
   se <- model$se[1]
   ci <- model$ci[1,]
-  return(list(ate=ate,se=se,ci=ci))
+  return(list(ate=ate,se=se,ci.lower=ci[1],ci.upper=ci[2]))
 }
 
 rdd_IW <- function(Y,X,c=0) {
@@ -15,7 +15,7 @@ rdd_IW <- function(Y,X,c=0) {
   ate <- model$tau.hat
   se <- model$sampling.se
   ci <- model$tau.hat +c(-1,1)*model$tau.plusminus
-  return(list(ate=ate,se=se,ci=ci))
+  return(list(ate=ate,se=se,ci.lower=ci[1],ci.upper=ci[2]))
 }
 
 rdd_AK <- function(Y,X,c=0) {
@@ -23,7 +23,7 @@ rdd_AK <- function(Y,X,c=0) {
   ate <- model$estimate
   se <- as.numeric(model$sd)
   ci <- c(as.numeric(model$lower),as.numeric(model$upper))
-  return(list(ate=ate,se=se,ci=ci))
+  return(list(ate=ate,se=se,ci.lower=ci[1],ci.upper=ci[2]))
 }
 
 rdd_QD<- function(Y,X,c=0) {
@@ -31,5 +31,5 @@ rdd_QD<- function(Y,X,c=0) {
   ate <- model$coef[3]
   se <- model$se[3]
   ci <- ate+c(-1,1)*1.96*se
-  return(list(ate=ate,se=se,ci=ci))
+  return(list(ate=ate,se=se,ci.lower=ci[1],ci.upper=ci[2]))
 }
