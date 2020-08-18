955ea786a48710bd4a549352f1000153d0354986
Evan Munro <evan.munro@gmail.com>
2020-08-17 21:28:26 -0700

    tables test1

--- make_tables.R
+++ make_tables.R
@@ -1,13 +1,23 @@
+library(kableExtra)
 args = commandArgs(TRUE)
-outname =  args[1]
+name =  args[1]
 estimators = c("rddIK", "rddLLRM", "rddLLRC", "rddIW", "rddAK", "rddQD", "rddBayes")
 
 outpath  = paste0("output/", outname)
 tablepath = paste0("tables/", outname, ".txt")
 
-sink(tablepath)
+files = list.files(outpath)
+print(files)
+#samples = rbind(lapply(files, FUN = function(x) return read.csv(x, row.names=estimators))
+#colnames(samples) = c("ate", "ci.l", "ci.u")
 
-sink()
+#collate_samples  <- function(est.name, samples) {
+  #
 
-file_list =
+#}
+#sink(tablepath)
+
+#sink()
+
+#file_list =
 rbind()
