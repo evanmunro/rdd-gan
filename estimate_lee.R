57cccafb8844a421c958ab6cb472fbc09a07fe5a
Evan Munro <evan.munro@gmail.com>
2020-10-30 18:17:30 -0700

    move julia simulation to MinMaxRDD

--- estimate_lee.R
+++ estimate_lee.R
@@ -1,10 +1,12 @@
-data <- head(read.csv("data/cleaned/lee.csv"), 500)
-
+data <- read.csv("data/cleaned/lee.csv")
+data <- data[abs(data$x) < 0.18,]
 X = data$x
 Y= data$y
 c= 0 
 M= RDHonest::NPR_MROT.fit(RDHonest::RDData(data[,c("y","x")], cutoff=0))
+sigma = 
 W = as.numeric(X>c)
 model <-  optrdd::optrdd(X,Y,W,
                          max.second.derivative=M,verbose=F,estimation.point=c,
-                         try.elnet.for.sigma.sq=T,optimizer='mosek')
+                         sigma.sq = 0.0118858279376246, 
+                         try.elnet.for.sigma.sq=F, use.spline=F, optimizer='mosek')
