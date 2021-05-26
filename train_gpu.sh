710b13788ae7a9f1fa82f77ad6da59749f50ed24
Evan Munro <evan.munro@gmail.com>
2021-05-26 11:40:20 -0700

    add pandas loading

--- train_gpu.sh
+++ train_gpu.sh
@@ -9,4 +9,5 @@ module load python/3.6.1
 module load cuda/11.2.0
 module load py-pytorch/1.4.0_py36
 module load py-numpy/1.19.2_py36
+module load py-pandas/1.0.3_py36
 python3 generation/estimate_gans.py
