a0e13c34bc91c8a4cd0c8594af5d2c3d5aa61ea4
Evan Munro <evan.munro@gmail.com>
2021-08-24 14:44:53 -0700

    add estimate_new

---
+++ generation/estimate_new.py
@@ -0,0 +1,19 @@
+from rdgan import GanRDD
+import pandas as pd
+
+def run_training_loop(dname, xbound, ybound):
+    data = pd.read_csv("data/cleaned/"+dname+".csv")
+    print(dname)
+    ganModel = GanRDD("dname", data, xbound = xbound, ybound = ybound, epochs=2000)
+    #leeGAN.load()
+    ganModel.train()
+    ganModel.save_models()
+
+    print("GROUND TRUTH: ")
+    print(ganModel.groundTruth())
+    ganModel.evaluate_results()
+    ganModel.generate_data(save=True, sample_size = 1e7)
+
+run_training_loop("meyersson", [-100, 100], [0, 100])
+run_training_loop("senate", [-100, 100], [0, 100])
+run_training_loop("brazil", [-100, 100], [0, 100])
